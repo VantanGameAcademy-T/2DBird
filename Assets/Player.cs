7470aaa9e59f257c6dbb4cd409679c3fba5e911b
VGA-NPC-2022\teacher <vga.teacher@gmail.com>
2021-07-10 14:45:23 +0900

    20121/7/3までの分

---
+++ Assets/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float JumpVelocity = 10;  // ジャンプ力
+
+    Rigidbody2D rb2d;
+
+    [SerializeField]
+    private Sprite sp;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+        TouchManager.Began += (info) => 
+        {
+            rb2d.velocity = Vector2.zero; // 落下速度リセットする
+            rb2d.AddForce(transform.up * JumpVelocity, ForceMode2D.Impulse);    // 上方向に力を加える
+        };
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        Destroy(GetComponent<Animator>());
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        renderer.sprite = sp;
+    }
+
+}
---
+++ Assets/Wall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x - 0.02f,
+                transform.position.y,
+                transform.position.z);
+
+        if (transform.position.x < -9.8f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
