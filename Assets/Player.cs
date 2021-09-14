3bbb8a2b5aa5047b4021f106a64e2fbb884d394c
VgatTeacher <vgat.teacher@gmail.com>
2021-09-14 18:52:59 +0900

    課題対応

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,31 +8,49 @@ public class Player : MonoBehaviour
     float JumpVelocity = 10;  // ジャンプ力
 
     Rigidbody2D rb2d;
+    Animator thisAinme;
+    SpriteRenderer thisRenderer;
 
     [SerializeField]
     private Sprite sp;
 
+    private static bool isDead = false;
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        thisAinme = GetComponent<Animator>();
+        thisRenderer = GetComponent<SpriteRenderer>();
 
-    }
-
-    void Update()
-    {
-        TouchManager.Began += (info) => 
+        TouchManager.Began += (info) =>
         {
             rb2d.velocity = Vector2.zero; // 落下速度リセットする
             rb2d.AddForce(transform.up * JumpVelocity, ForceMode2D.Impulse);    // 上方向に力を加える
         };
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void Update()
     {
-        
-        Destroy(GetComponent<Animator>());
-        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
-        renderer.sprite = sp;
+        if (isDead)
+        {
+            StartCoroutine(WaitGame(1.0f));
+        }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+    }
+
+    IEnumerator WaitGame(float second)
+    {
+        thisAinme.enabled = false;
+        thisRenderer.sprite = sp;
+        rb2d.simulated = false;
+        yield return new WaitForSeconds(second);
+
+        thisAinme.enabled = true;
+        rb2d.simulated = true;
+        isDead = false;
+    }
 }
--- Assets/WallManager.cs
+++ Assets/WallManager.cs
@@ -24,7 +24,6 @@ public class WallManager : MonoBehaviour
             GameObject obj = (GameObject)Instantiate(walls[id], 
                 spawner.transform.position, 
                 spawner.transform.rotation);
-
         }
     }
 }
